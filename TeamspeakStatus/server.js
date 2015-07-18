97cd26cddce44f858eafcf43dbb90540884acdec
3ventic <3ventic@gmail.com>
2015-07-18 19:52:50 +0300

    added files

---
+++ TeamspeakStatus/server.js
@@ -0,0 +1,130 @@
+﻿var http = require('http');
+var teamspeak = require('node-teamspeak');
+var config = require('./config.js');
+
+var port = process.env.port || 1337;
+
+var status = {
+    clients: 0,
+    max_clients: 0,
+    uptime: 0,
+    channels: []
+};
+
+
+http.createServer(function (req, res)
+{
+    res.writeHead(200, { 'Content-Type': 'application/json;charset=utf-8' });
+    res.end(JSON.stringify(status));
+}).listen(port);
+console.log('Listening on port ' + port);
+
+
+var tsclient = new teamspeak(config.address);
+tsclient.send('login', {
+    client_login_name: config.username,
+    client_login_password: config.password
+}, function (err, response)
+{
+    if (err)
+    {
+        console.log(err);
+        return;
+    }
+    tsclient.send('use', { sid: 1 }, function (err, response)
+    {
+        if (err)
+        {
+            console.log(err);
+            return;
+        }
+        updateStatus();
+        setInterval(updateStatus, 30000);
+    });
+});
+
+function updateStatus()
+{
+    tsclient.send('hostinfo', function (err, response)
+    {
+        if (err)
+        {
+            console.log(err);
+            return;
+        }
+        status.clients = response.virtualservers_total_clients_online;
+        status.max_clients = response.virtualservers_total_maxclients;
+        status.uptime = response.instance_uptime;
+    });
+    tsclient.send('channellist', function (err, response)
+    {
+        if (err)
+        {
+            console.log(err);
+            return;
+        }
+        
+        var num_chans = response.length;
+        var channels = [];
+        
+        function findParentAndAddChild(chans, child)
+        {
+            for (var i = 0; i < chans.length; ++i)
+            {
+                if (chans[i].cid === child.pid)
+                {
+                    chans[i].children.push(child);
+                    chans[i].children[chans[i].children.length - 1].children = [];
+                    return true;
+                }
+                else if (findParentAndAddChild(chans[i].children, child))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        
+        for (var i = 0; i < num_chans; ++i)
+        {
+            if (response[i].pid === 0)
+            {
+                channels.push(response[i]);
+                channels[channels.length - 1].children = [];
+            }
+            else
+            {
+                findParentAndAddChild(channels, response[i]);
+            }
+        }
+        
+        tsclient.send('clientlist', function (err, response)
+        {
+            if (err)
+            {
+                console.log(err);
+                return;
+            }
+            
+            function addUsersToChannels(chans)
+            {
+                for (var i = 0; i < chans.length; ++i)
+                {
+                    chans[i].users = [];
+                    for (var j = 0; j < response.length; ++j)
+                    {
+                        if (response[j].client_type === 0 && response[j].cid === chans[i].cid)
+                        {
+                            chans[i].users.push(response[j].client_nickname);
+                        }
+                    }
+                    addUsersToChannels(chans[i].children);
+                }
+            }
+
+            addUsersToChannels(channels);
+
+            status.channels = channels;
+        });
+    });
+}
